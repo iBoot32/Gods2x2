817bb313f0e49e96aaaa803138c677833e6aa57f
iBoot32 <iboot32@gmail.com>
2020-07-31 19:52:15 -0400

    add files

---
+++ Gods2x2/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Gods2x2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.heading = new System.Windows.Forms.Label();
+            this.author = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statebox = new System.Windows.Forms.TextBox();
+            this.console = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // heading
+            // 
+            this.heading.Font = new System.Drawing.Font("Tahoma", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heading.Location = new System.Drawing.Point(9, 11);
+            this.heading.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.heading.Name = "heading";
+            this.heading.Size = new System.Drawing.Size(432, 98);
+            this.heading.TabIndex = 0;
+            this.heading.Text = "God\'s 2x2";
+            this.heading.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // author
+            // 
+            this.author.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.author.Location = new System.Drawing.Point(79, 711);
+            this.author.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.author.Name = "author";
+            this.author.Size = new System.Drawing.Size(299, 27);
+            this.author.TabIndex = 1;
+            this.author.Text = "By: Tom O\'Donnell";
+            this.author.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(364, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "A program to brute-force a solution to a 2x2 Rubik\'s Cube";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(16, 646);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(425, 49);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statebox
+            // 
+            this.statebox.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.statebox.Location = new System.Drawing.Point(16, 602);
+            this.statebox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.statebox.Name = "statebox";
+            this.statebox.Size = new System.Drawing.Size(424, 22);
+            this.statebox.TabIndex = 5;
+            this.statebox.Text = "Enter Cubestate Here";
+            this.statebox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // console
+            // 
+            this.console.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.console.Location = new System.Drawing.Point(16, 113);
+            this.console.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.console.Name = "console";
+            this.console.ReadOnly = true;
+            this.console.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.console.Size = new System.Drawing.Size(424, 468);
+            this.console.TabIndex = 6;
+            this.console.Text = "Console Window";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 738);
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.statebox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.author);
+            this.Controls.Add(this.heading);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "God\'s 2x2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label heading;
+        private System.Windows.Forms.Label author;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox statebox;
+        private System.Windows.Forms.RichTextBox console;
+    }
+}
+
---
+++ Gods2x2/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gods2x2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string cubestate;
+        public static List<string> moves = new List<string>();
+        public static string temp;
+        public static string[] possiblemoves = new string[12] { "R", "R'", "L", "L'", "U", "U'", "D", "D'", "F", "F'", "B", "B'"};
+        Random random = new Random();
+        public List<string> mov = new List<string>();
+        public static string backup;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            moves.Add("N/A");
+            update.cs = statebox.Text;
+            con("");
+            con("Cubestate is: " + statebox.Text);
+            console.Refresh();
+
+
+            //begin preparatory work (checking pieces)
+
+            if (solved())
+            {
+                done();
+                return;
+            }
+
+            //Stage 1
+            if (update.cs.Substring(18, 3) != update.corner.Substring(18, 3) || update.cs.Substring(21, 3) != update.corner.Substring(21, 3))
+            {
+                while (update.cs.Substring(18, 3) != update.corner.Substring(18, 3) || update.cs.Substring(21, 3) != update.corner.Substring(21, 3))
+                {
+                    backup = update.cs;
+                    gen(4);
+                    foreach (var turn in mov)
+                    {
+                        update.manipulate(turn);
+                    }
+
+                    if (update.cs.Substring(18, 3) != update.corner.Substring(18, 3) || update.cs.Substring(21, 3) != update.corner.Substring(21, 3))
+                    {
+                        update.cs = backup; //restore original state and try again
+                        mov.Clear();
+                    }
+                }
+                con(Environment.NewLine + "Stage 1 solved using moves: ");
+                foreach (var VARIABLE in mov)
+                {
+                    con(VARIABLE);
+                }
+                console.Refresh();
+            }
+
+            //Stage 2
+            if (update.cs.Substring(15, 3) != update.corner.Substring(15, 3))
+            {
+                while (update.cs.Substring(15, 3) != update.corner.Substring(15, 3) || update.cs.Substring(18, 3) != update.corner.Substring(18, 3) || update.cs.Substring(21, 3) != update.corner.Substring(21, 3))
+                {
+                    backup = update.cs;
+                    gen(4);
+                    foreach (var turn in mov)
+                    {
+                        update.manipulate(turn);
+                    }
+
+                    if (update.cs.Substring(15, 3) != update.corner.Substring(15, 3) || update.cs.Substring(18, 3) != update.corner.Substring(18, 3) || update.cs.Substring(21, 3) != update.corner.Substring(21, 3))
+                    {
+                        update.cs = backup; //restore original state and try again
+                        mov.Clear();
+                    }
+                }
+                con(Environment.NewLine + "Stage 2 solved using moves: ");
+                foreach (var VARIABLE in mov)
+                {
+                    con(VARIABLE);
+                }
+            }
+            else
+            {
+                con(Environment.NewLine + "Stage 2 is already completed.");
+            }
+            console.Refresh();
+
+            //Stage 3
+            if (update.cs.Substring(12, 3) != update.corner.Substring(12, 3))
+            {
+                while (update.cs.Substring(12, 3) != update.corner.Substring(12, 3) || update.cs.Substring(15, 3) != update.corner.Substring(15, 3) || update.cs.Substring(18, 3) != update.corner.Substring(18, 3) || update.cs.Substring(21, 3) != update.corner.Substring(21, 3))
+                {
+                    backup = update.cs;
+                    gen(8);
+                    foreach (var turn in mov)
+                    {
+                        update.manipulate(turn);
+                    }
+
+                    if (update.cs.Substring(12, 3) != update.corner.Substring(12, 3) || update.cs.Substring(15, 3) != update.corner.Substring(15, 3) || update.cs.Substring(18, 3) != update.corner.Substring(18, 3) || update.cs.Substring(21, 3) != update.corner.Substring(21, 3))
+                    {
+                        update.cs = backup; //restore original state and try again
+                        mov.Clear();
+                    }
+                }
+                con(Environment.NewLine + "Stage 3 solved using moves: ");
+                foreach (var VARIABLE in mov)
+                {
+                    con(VARIABLE);
+                }
+            }
+            else
+            {
+                con(Environment.NewLine + "Stage 3 is already completed.");
+            }
+            console.Refresh();
+
+            //Stage 4- Get TFR permuted
+            mov.Clear();
+            if (!update.cs.Substring(6, 3).Contains("W") || !update.cs.Substring(6, 3).Contains("R") ||
+                !update.cs.Substring(6, 3).Contains("G"))
+            {
+                while (!update.cs.Substring(6, 3).Contains("W") || !update.cs.Substring(6, 3).Contains("R") ||
+                       !update.cs.Substring(6, 3).Contains("G")) //if TFR is not permuted
+                {
+                    update.manipulate("U");
+                    mov.Add(possiblemoves[4]);
+                }
+
+                con(Environment.NewLine + "TFR Permuted using moves: ");
+
+                foreach (var VARIABLE in mov)
+                {
+                    con(VARIABLE);
+                }
+            }
+            else
+            {
+                con(Environment.NewLine + "TFR is permuted already");
+            }
+            console.Refresh();
+
+            //stage 5- permute other corners
+            mov.Clear();
+            if (!permuted(0) || !permuted(3) || !permuted(9)) //if anything is not permuted
+            {
+                while (!permuted(0) || !permuted(3) || !permuted(9))
+                {
+                    update.manipulate("U"); update.manipulate("R"); update.manipulate("U'"); update.manipulate("L'"); update.manipulate("U"); update.manipulate("R'"); update.manipulate("U'"); update.manipulate("L");
+                    mov.Add("U R U' L' U R' U' L ");
+
+                    if (permuted(6) && permuted(9))
+                    {
+                        break;
+                    }
+                }
+
+                con(Environment.NewLine + "Corners are permuted using moves:");
+                foreach (var move in mov)
+                {
+                    con(move);
+                }
+            }
+            else
+            {
+                con(Environment.NewLine + "Corners are already permuted");
+            }
+
+            mov.Clear();
+            //if back two still aren't permuted
+            if (!permuted(0))
+            {
+                mov.Add("U R U R' U' R' F R2 U' R' U' R U R' F' U'");
+                string[] spl = "U R U R' U' R' F R2 U' R' U' R U R' F' U'".Split(' ');
+                foreach (var move in spl)
+                {
+                    update.manipulate(move);
+                }
+                con("Back corners fixed with:  U R U R' U' R' F R2 U' R' U' R U R' F' U'");
+            }
+
+            mov.Clear();
+
+            for (int i = 0; i < 4; i++)
+            {
+                while (update.cs.Substring(6, 1) != "W")
+                {
+                    mov.Add("R' D' R D ");
+                    update.manipulate("R'"); update.manipulate("D'"); update.manipulate("R"); update.manipulate("D");
+                }
+                con("Next corner solved using moves:");
+                foreach (var item in mov)
+                {
+                    con(item);
+                }
+                con("U");
+                update.manipulate("U");
+                mov.Clear();
+            }
+
+            con(" "); con(" ");
+            con("Your cube has been solved!");
+        }
+
+        public bool permuted(int sticker)
+        {
+            if (update.cs.Substring(sticker, 3).Contains(update.corner[sticker]) &&
+                update.cs.Substring(sticker, 3).Contains(update.corner[sticker + 1]) &&
+                update.cs.Substring(sticker, 3).Contains(update.corner[sticker + 2]))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public bool solved()
+        {
+            if (update.cs == update.corner)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void gen(int number_of_moves)
+        {
+            for (int i = 0; i < number_of_moves; i++)
+            {
+                mov.Add(possiblemoves[random.Next(0, 11)]);
+            }
+        }
+        public void done()
+        {
+            con("");
+            con("Cube is solved using moves: ");
+            foreach (var item in moves)
+            {
+                temp += item + " ";
+            }
+            con(temp);
+        }
+
+        public void con(string text)
+        {
+            console.AppendText(Environment.NewLine + "  " + text);
+            console.ScrollToCaret();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
